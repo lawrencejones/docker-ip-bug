7b147b92dfe35116c1234161a0a60f2f2afb5a9e
Lawrence Jones <lawrjone@gmail.com>
2017-05-03 11:15:16 +0100

    Initial commit

---
+++ provision.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Configures apt with dockers repository and installs docker
+
+DOCKER_VERSION="1.12.6-0~ubuntu-trusty"
+
+apt-get update
+apt-get install -y curl ca-certificates apt-transport-https
+
+add-apt-repository https://apt.dockerproject.org/repo
+apt-key adv --keyserver p80.pool.sks-keyservers.net --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-get update
+
+apt-get install -y "docker-engine=$DOCKER_VERSION"
+update-grub
