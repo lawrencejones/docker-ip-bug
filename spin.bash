6e49d5e5698f729b65901864d3f06c77efe6b84d
Lawrence Jones <lawrjone@gmail.com>
2017-05-04 11:53:30 +0100

    Better busy

--- spin.bash
+++ spin.bash
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 # Busy spin for a random period of time, then start netcat
 
-COUNT=$(((RANDOM % 10) + 5))
 IP_ADDRESS=$(ip addr show eth0 | perl -wnl -e '/inet ([^\/]+)/ and print $1')
 
-echo "[$(date)] [$$] [$IP_ADDRESS] Do something busy $COUNT times..."
-while [ $((--COUNT)) != 0 ]; do
-  # Do something busy for about 125ms
-  head -n 512 /dev/urandom | bzip2 -5 >> /dev/null
-  sleep 0.01
-done
+echo "[$(date)] [$$] [$IP_ADDRESS] Do something busy..."
+# Do something busy for a bit
+head -n 8192 /dev/urandom | bzip2 -5 >> /dev/null
 echo "[$(date)] [$$] [$IP_ADDRESS] Stop spinning, now listen!"
 
 while true; do
