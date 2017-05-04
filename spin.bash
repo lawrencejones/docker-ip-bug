7fe993855c758422b01b3f900f227d85e0616e06
Lawrence Jones <lawrjone@gmail.com>
2017-05-04 11:55:51 +0100

    Optional spin

--- spin.bash
+++ spin.bash
@@ -3,11 +3,14 @@
 
 IP_ADDRESS=$(ip addr show eth0 | perl -wnl -e '/inet ([^\/]+)/ and print $1')
 
-echo "[$(date)] [$$] [$IP_ADDRESS] Do something busy..."
-# Do something busy for a bit
-head -n 8192 /dev/urandom | bzip2 -5 >> /dev/null
-echo "[$(date)] [$$] [$IP_ADDRESS] Stop spinning, now listen!"
+if [ "$1" == "--spin" ]; then
+  echo "[$(date)] [$$] [$IP_ADDRESS] Do something busy..."
+  # Do something busy for a bit
+  head -n 8192 /dev/urandom | bzip2 -9 >> /dev/null
+  echo "[$(date)] [$$] [$IP_ADDRESS] Stop spinning, now listen!"
+fi
 
+echo "[$(date)] [$$] [$IP_ADDRESS] Serving on port 8080..."
 while true; do
   nc -l 8080 <<< "done"
 done
