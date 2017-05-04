2d6fe113cadb2651331b9bd29c448f9b20838512
Chris Sinjakli <chris@gocardless.com>
2017-05-04 15:02:24 +0100

    Fix adding apt repo in provision script

--- provision.bash
+++ provision.bash
@@ -6,7 +6,7 @@ DOCKER_VERSION="1.12.6-0~ubuntu-trusty"
 apt-get update
 apt-get install -y curl ca-certificates apt-transport-https jq ruby
 
-add-apt-repository https://apt.dockerproject.org/repo ubuntu-trusty
+add-apt-repository 'deb https://apt.dockerproject.org/repo ubuntu-trusty main'
 apt-key adv --keyserver p80.pool.sks-keyservers.net --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 apt-get update
 
